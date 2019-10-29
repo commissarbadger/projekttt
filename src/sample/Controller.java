95c3e0075bb42858e923f6efb7b3f809bce2f5b0
commissarbadger <jan.stypczynski@wp.pl>
2019-10-29 10:36:13 +0100

    project commit 3 - submarine

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,5 +3,7 @@ package sample;
 public class Controller {
     public static void main (String[] args){
 System.out.println("submarine");
+        System.out.println("submarine");
+    System.out.println("submarine");
     }
 }
